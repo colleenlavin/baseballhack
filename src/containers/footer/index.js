7260f93c46ba9810b07aa1068dd99b4a50315bf1
Colleen Lavin <cml388@mun.ca>
2018-03-17 12:47:02 -0700

    Adds more basic outline

---
+++ src/containers/footer/index.js
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+export default () => (
+  <div>
+     <footer className="App-header">
+     <h1 className="App-title">Footer Stuff</h1>
+   </footer>
+  </div>
+)
---
+++ src/containers/header/index.js
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+export default () => (
+  <div>
+     <header className="App-header">
+     <h1 className="App-title">Math Games</h1>
+   </header>
+  </div>
+)
