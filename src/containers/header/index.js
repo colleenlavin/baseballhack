d479fe60b144815e340a1173c67d23f2cbb53d73
JayaArasalike <mail@jayalaxmi.info>
2018-03-17 18:45:14 -0700

    logo font color changed

--- src/containers/header/index.js
+++ src/containers/header/index.js
@@ -5,7 +5,7 @@ import './style.css';
 export default () => (
   <div>
     <div className="header clearfix">
-    <h3 className="text-muted">Statball</h3>
+    <h3 className="text-muted" id="ja">Statball</h3>
   </div>
   </div>
 )
