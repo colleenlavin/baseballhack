f959827b0e56e3d41704148f549cb5cf9a35e8c6
Colleen Lavin <cml388@mun.ca>
2018-03-17 18:37:03 -0700

    push already

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -8,7 +8,9 @@ const Home = props => (
 
   <div className="mainBox">
     <h1>When am I ever going to use math?</h1>
+    <div>
     <img src="http://mopress.com/wp-content/uploads/2017/06/Baseball-Photo.jpg" />
+    </div>
     <button onClick={() => props.changePage()}>What does any of this mean?</button>
   </div>
 )
--- src/containers/q3/index.js
+++ src/containers/q3/index.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux'
 
 const Q3 = props => (
   <div>
-    <h1>Based on the pitcher's 5 last games, calculate the Interquartile range of the pitcher's strikes. </h1>
+    <h1>Based on Kershaw's 5 last games, calculate the Interquartile range of the pitcher's strikes. </h1>
     <input type="text"></input>
     <button onClick={() => props.changePage()}>Next</button>
   </div>
